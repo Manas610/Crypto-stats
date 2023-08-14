e3a3959ffc35cab710fca2962b47e8b3d1e43d71
Manas <singhmanas610@gmail.com>
2023-08-14 23:08:44 +0530

    hover color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,13 @@ import "./header.css"
 const Header = () => {
   return (
     <HStack p={"4"} shadow={"base"} backgroundColor={"blackAlpha.900"}>
-        <Button variant={"unstyled"} color={"white"} pr={"4"}>
+        <Button variant={"unstyled"} color={"white"} pr={"4"} className='button'>
             <NavLink to="/">Home</NavLink>
         </Button>
-        <Button variant={"unstyled"} color={"white"} pr={"4"}>
+        <Button variant={"unstyled"} color={"white"} pr={"4"} className='button'>
             <NavLink to="/exchanges">Exchanges</NavLink>
         </Button>
-        <Button variant={"unstyled"} color={"white"}>
+        <Button variant={"unstyled"} color={"white"} className='button'>
             <NavLink to="/coins">Coins</NavLink>
         </Button>
     </HStack>
